e431db1bb76eac74b62495b80c76efa0806ef3e9
vince <paulag10987@gmail.com>
2025-12-07 22:00:27 +0800

    chore(main,py/Snake): add base code

---
+++ Snake/main.py
@@ -0,0 +1,32 @@
+import pygame
+import sys
+
+from pygame.locals import *
+from pygame.sprite import Sprite
+
+class Snake(Sprite):
+    def __init__(self):
+        super().__init__()
+        pass
+
+    def update(self):
+        pass
+
+class Apple(Sprite):
+    def __init__(self):
+        super().__init__()
+        pass
+
+    def update(self):
+        pass
+
+class Main:
+    def __init__(self):
+        pass
+
+    def run(self):
+        pass
+
+if __name__ == "__main__":
+    game = Main()
+    game.run()
