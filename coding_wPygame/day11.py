82d34a53ef98a8256a9a971a2b909e612e0c637d
vince <paulag10987@gmail.com>
2025-12-08 16:04:15 +0800

    chore(day11.py): add camera functions

--- coding_wPygame/day11.py
+++ coding_wPygame/day11.py
@@ -1,6 +1,7 @@
 import pygame
 import sys
 
+from PIL.ImageChops import offset
 from pygame.locals import *
 
 # ============= UI COLORS ============
@@ -30,17 +31,56 @@ class Player(pygame.sprite.Sprite):
 class CameraGroup(pygame.sprite.Group):
     def __init__(self):
         super().__init__()
-        pass
 
-    def update(self):
-        pass
+        self.CAMERA_DISPLAY_SURFACE = pygame.display.get_surface()
+
+        self.OFFSET = pygame.math.Vector2()
+        self.ZOOM_SCALE = 1.0
+        self.INTERNAL_SURFACE_SIZE = (2500,2500)
+        self.INTERNAL_SURFACE = pygame.Surface(self.INTERNAL_SURFACE_SIZE, pygame.SRCALPHA)
+        self.INTERNAL_RECT = self.INTERNAL_SURFACE.get_rect(center = (Main.DISPLAY_WIDTH // 2, Main.DISPLAY_HEIGHT // 2))
+
+
+    def custom_draw(self, player):
+
+        keys = pygame.key.get_pressed()
+
+        if keys[K_q]:
+            self.ZOOM_SCALE += 0.05
+        if keys[K_e]:
+            self.ZOOM_SCALE -= 0.05
+
+        if self.ZOOM_SCALE < 0.3:
+            self.ZOOM_SCALE = 0.3
+        if self.ZOOM_SCALE > 2.0:
+            self.ZOOM_SCALE = 2.0
+
+        self.OFFSET.x = player.rect.centerx - Main.DISPLAY_WIDTH // 2
+        self.OFFSET.y = player.rect.centery - Main.DISPLAY_HEIGHT // 2
+
+        for sprite in sorted(self.sprites(), key = lambda  s: s.rect.centery):
+
+            OFFSET_POS = sprite.rect.topleft - self.OFFSET
+            CENTER_OFFSET = OFFSET_POS - pygame.math.Vector2(Main.DISPLAY_WIDTH // 2, Main.DISPLAY_HEIGHT // 2)
+            ZOOMED_OFFSET = CENTER_OFFSET * self.ZOOM_SCALE
+            FINAL_POS = ZOOMED_OFFSET + pygame.math.Vector2(Main.DISPLAY_WIDTH // 2, Main.DISPLAY_HEIGHT // 2)
+
+            new_W = int(sprite.rect.width * self.ZOOM_SCALE)
+            new_H = int(sprite.rect.height * self.ZOOM_SCALE)
+
+            if -100 < FINAL_POS.x < Main.DISPLAY_WIDTH + 100 and -100 < FINAL_POS.y < Main.DISPLAY_HEIGHT + 100:
+                ZOOMED_IMAGE = pygame.transform.scale(sprite.image, (new_W, new_H))
+                self.CAMERA_DISPLAY_SURFACE.blit(ZOOMED_IMAGE, FINAL_POS)
+
 
 class Main:
+
+    DISPLAY_WIDTH = 800
+    DISPLAY_HEIGHT = 600
+
     def __init__(self):
 
         pygame.init()
-        self.DISPLAY_WIDTH = 800
-        self.DISPLAY_HEIGHT = 600
         self.DISPLAY = pygame.display.set_mode((self.DISPLAY_WIDTH, self.DISPLAY_HEIGHT))
 
         self.CLOCK = pygame.time.Clock()
