b8dbdf53c968b307318bb9b9e8345be7ef34bf62
vince <paulag10987@gmail.com>
2025-12-17 14:02:55 +0800

    chore(main.py/spaceinvaders): add main game loop

--- SpaceInvaders/main.py
+++ SpaceInvaders/main.py
@@ -4,11 +4,30 @@ import sys
 from pygame.locals import *
 
 class Main:
+
+    DISPLAY_WIDTH = 800
+    DISPLAY_HEIGHT = 600
+    DISPLAY_COLOR = (0,0,0)
+
     def __init__(self):
-        pass
+
+        pygame.init()
+        self.DISPLAY = pygame.display.set_mode((self.DISPLAY_WIDTH, self.DISPLAY_HEIGHT))
+
+        self.CLOCK = pygame.time.Clock()
+        self.FPS = 60
 
     def run(self):
-        pass
+
+        while True:
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+            self.DISPLAY.fill(self.DISPLAY_COLOR)
+            pygame.display.set_caption("SPACE INVADERS")
+            pygame.display.update()
 
 if __name__ == "__main__":
     app = Main()
