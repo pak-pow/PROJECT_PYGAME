4543c8cec077ddc6fe3309fa6bfdafbea0d7293f
vince <paulag10987@gmail.com>
2025-12-18 14:22:34 +0800

    chore(day21): add player logic and friction physics

--- coding_wPygame/day21.py
+++ coding_wPygame/day21.py
@@ -3,6 +3,57 @@ import sys
 
 from pygame.locals import *
 
+class Player(pygame.sprite.Sprite):
+
+    def __init__(self):
+        super().__init__()
+
+        self.image = pygame.Surface((40,40))
+        self.image.fill((0,255,0))
+
+        self.rect = self.image.get_rect(center = (400,300))
+        self.pos = pygame.Vector2(self.rect.center)
+        self.vel = pygame.Vector2()
+
+        self.acceleration = 3000
+        self.max_speed = 300
+        self.friction = 0.95
+
+        self.speed = 400
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+        direction = pygame.Vector2(0, 0)
+
+        if keys[K_w]:
+            direction.y -= 1
+
+        if keys[K_s]:
+            direction.y += 1
+
+        if keys[K_a]:
+            direction.x -= 1
+
+        if keys[K_d]:
+            direction.x += 1
+
+        if direction.length_squared() > 0:
+            direction = direction.normalize()
+            self.vel += direction * self.acceleration * dt
+
+        else:
+            self.vel *= self.friction
+
+        if self.vel.length() > self.max_speed:
+            self.vel.scale_to_length(self.max_speed)
+
+        self.pos += self.vel * dt
+
+        self.pos.x = max(20, min(Main.DISPLAY_WIDTH - 20, self.pos.x))
+        self.pos.y = max(20, min(Main.DISPLAY_HEIGHT - 20, self.pos.y))
+
+        self.rect.center = self.pos
+
 class Main:
 
     DISPLAY_WIDTH= 800
@@ -20,6 +71,11 @@ class Main:
 
     def run(self):
 
+        all_sprite = pygame.sprite.Group()
+        player = Player()
+
+        all_sprite.add(player)
+
         while True:
 
             dt = self.CLOCK.tick(self.FPS) / 1000
@@ -29,7 +85,10 @@ class Main:
                     pygame.quit()
                     sys.exit()
 
+            all_sprite.update(dt)
             self.DISPLAY.fill(self.DISPLAY_COLOR)
+
+            all_sprite.draw(self.DISPLAY)
             pygame.display.update()
 
 if __name__ == "__main__":
