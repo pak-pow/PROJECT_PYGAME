5e43e20816a6b20aa10140413ffdee11e23f06bd
vince <paulag10987@gmail.com>
2025-12-20 14:20:29 +0800

    chore(main.py/flappybird): add pipe logic and graphics

--- FlappyBird/main.py
+++ FlappyBird/main.py
@@ -1,5 +1,6 @@
 import pygame
 import sys
+import random
 
 from pygame.locals import *
 
@@ -10,32 +11,52 @@ class Player(pygame.sprite.Sprite):
         original_image = pygame.image.load("bird.png").convert_alpha()
         self.image = pygame.transform.scale(original_image, (40, 40))
 
-        # 1. Start centered
         self.rect = self.image.get_rect(center=(Main.DISPLAY_WIDTH // 2, Main.DISPLAY_HEIGHT // 2))
         self.pos = pygame.Vector2(self.rect.center)
         self.velocity = pygame.Vector2()
 
     def jump(self):
-        # This function is called only when we press the key
         self.velocity.y = Main.JUMP_STRENGTH
 
     def update(self, dt):
-        # 2. Gravity always applies
         self.velocity.y += Main.GRAVITY * dt
         self.pos += self.velocity * dt
 
-        # 3. Floor Collision
         if self.pos.y >= Main.FLOOR_Y:
             self.pos.y = Main.FLOOR_Y
             self.velocity.y = 0
 
-        # 4. Ceiling Clamp (Don't let bird fly over the title bar)
         if self.pos.y < 0:
             self.pos.y = 0
             self.velocity.y = 0
 
-        # 5. Consistency: Use center here because you used center in __init__
         self.rect.center = round(self.pos)
+
+class Pipe(pygame.sprite.Sprite):
+    def __init__(self, x, y, is_top):
+        super().__init__()
+
+        width = 100
+        height = 350
+
+        original = pygame.image.load("pipe.png").convert_alpha()
+
+        visible_rect = original.get_bounding_rect()
+        cropped_image = original.subsurface(visible_rect)
+
+        self.image = pygame.transform.scale(cropped_image, (width, height))
+
+        if is_top:
+            self.rect = self.image.get_rect(bottomleft=(x, y))
+
+        else:
+            self.image = pygame.transform.flip(self.image, False, True)
+            self.rect = self.image.get_rect(topleft=(x, y))
+
+    def update(self, dt):
+        self.rect.x -= 300 * dt
+        if self.rect.right < 0:
+            self.kill()
 
 class Main:
 
@@ -53,8 +74,8 @@ class Main:
     UI_RED = (231, 76, 60)
 
     DISPLAY_WIDTH = 800
-    DISPLAY_HEIGHT = 600
-    FLOOR_Y = 600
+    DISPLAY_HEIGHT = 500
+    FLOOR_Y = 490
 
     GRAVITY = 2000
     MOVE_SPEED = 800
@@ -68,6 +89,12 @@ class Main:
         pygame.init()
         pygame.display.set_caption("Flappy Bird")
         self.DISPLAY = pygame.display.set_mode((self.DISPLAY_WIDTH, self.DISPLAY_HEIGHT))
+
+        bg_raw = pygame.image.load("background.png").convert()
+        self.bg_image = pygame.transform.scale(bg_raw, (self.DISPLAY_WIDTH, self.DISPLAY_HEIGHT))
+
+        self.SPAWN_PIPE_EVENT = pygame.USEREVENT + 1
+        pygame.time.set_timer(self.SPAWN_PIPE_EVENT, 1500)
 
     def run(self):
 
@@ -88,8 +115,24 @@ class Main:
                     if event.key == K_SPACE:
                         player.jump()
 
+                if event.type == self.SPAWN_PIPE_EVENT:
+
+                    gap_size = 150  # Space between pipes
+                    gap_y = random.randint(gap_size + 50, self.DISPLAY_HEIGHT - gap_size - 50)
+
+                    top_pipe_y = gap_y - (gap_size // 2)
+                    bottom_pipe_y = gap_y + (gap_size // 2)
+
+                    spawn_x = self.DISPLAY_WIDTH + 10
+
+                    new_top = Pipe(spawn_x, top_pipe_y, is_top=True)
+                    new_bottom = Pipe(spawn_x, bottom_pipe_y, is_top=False)
+
+                    all_sprite.add(new_top)
+                    all_sprite.add(new_bottom)
+
             all_sprite.update(dt)
-            self.DISPLAY.fill(self.UI_BLACK)
+            self.DISPLAY.blit(self.bg_image, (0, 0))
 
             all_sprite.draw(self.DISPLAY)
             pygame.display.update()
