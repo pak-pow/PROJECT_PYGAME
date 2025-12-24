2b23be6ef910ac24882521bef00af7d298ad5726
vince <paulag10987@gmail.com>
2025-12-24 15:29:08 +0800

    chore(day26): add main class

--- coding_wPygame/day26.py
+++ coding_wPygame/day26.py
@@ -6,12 +6,32 @@ from OpenGL.GL import *
 from OpenGL.GLU import *
 
 class Main:
+
+    DISPLAY_WIDTH = 800
+    DISPLAY_HEIGHT = 600
+    CLOCK = pygame.time.Clock()
+    FPS = 60
+
     def __init__(self):
-        pass
+
+        pygame.init()
+        self.DISPLAY = pygame.display.set_mode((self.DISPLAY_WIDTH, self.DISPLAY_HEIGHT))
+        pygame.display.set_caption("DAY 26")
 
     def run(self):
-        pass
 
-if __name__ == "__name__":
+        dt = self.CLOCK.tick(self.FPS) / 1000
+
+        while True:
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    pygame.quit()
+                    sys.exit()
+                    quit()
+
+            self.DISPLAY.fill((0,0,0))
+            pygame.display.update()
+
+if __name__ == "__main__":
     app = Main()
     app.run()
