0e54d3c24b2a53009f02d11d9072488f1d8815ac
vince <paulag10987@gmail.com>
2025-12-24 15:21:50 +0800

    chore(day26): add base file and code

---
+++ coding_wPygame/day26.py
@@ -0,0 +1,17 @@
+import pygame
+import sys
+
+from pygame.locals import *
+from OpenGL.GL import *
+from OpenGL.GLU import *
+
+class Main:
+    def __init__(self):
+        pass
+
+    def run(self):
+        pass
+
+if __name__ == "__name__":
+    app = Main()
+    app.run()
