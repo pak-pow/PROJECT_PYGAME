7000dc995cd374a59f1ec85138bcf8869f36eda9
vince <paulag10987@gmail.com>
2026-01-04 20:02:41 +0800

    feat(tetris/main): add main class and main game loop

--- Tetris/main.py
+++ Tetris/main.py
@@ -5,12 +5,29 @@ from pygame.locals import *
 
 class Main:
 
+    DISPLAY_WIDTH = 600
+    DISPLAY_HEIGHT = 800
+    DISPLAY_COLOR = (10,10,10)
+
+    CLOCK = pygame.time.Clock()
+    FPS = 60
+
     def __init__(self):
-        pass
+
+        pygame.init()
+        pygame.display.set_caption("Tetris: Copy")
+        self.DISPLAY = pygame.display.set_mode((self.DISPLAY_WIDTH, self.DISPLAY_HEIGHT))
 
     def run(self):
-        pass
+        
+        while True:
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    pygame.quit()
+                    sys.exit()
 
+            self.DISPLAY.fill(self.DISPLAY_COLOR)
+            pygame.display.update()
 
 if __name__ == '__main__':
     app = Main()
