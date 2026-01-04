6dee549cd6a91663c0f07b23400c4fe5500cfeb3
vince <paulag10987@gmail.com>
2026-01-04 19:57:14 +0800

    feat(Tetris/main.py): add base code

---
+++ Tetris/main.py
@@ -0,0 +1,17 @@
+import pygame
+import sys
+
+from pygame.locals import *
+
+class Main:
+
+    def __init__(self):
+        pass
+
+    def run(self):
+        pass
+
+
+if __name__ == '__main__':
+    app = Main()
+    app.run()
